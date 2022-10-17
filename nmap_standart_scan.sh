ff28d3a75b9d3873770d125e71636668c298f879
setronn <setron34@gmail.com>
2022-10-17 05:44:19 -0400

    renamed tcp to udp in UDP scan

--- nmap_standart_scan.sh
+++ nmap_standart_scan.sh
@@ -3,5 +3,5 @@
 ports=$(nmap -p- -Pn $1 -oN $1.tcp.temp | grep ^[0-9] | cut -d '/' -f 1 | tr '\n' ',' | sed s/,$//)
 sudo nmap -p$ports -A $1 -oN $1.tcp
 
-ports=$(sudo nmap -sU -Pn $1 -oN $1.tcp.temp | grep ^[0-9] | cut -d '/' -f 1 | tr '\n' ',' | sed s/,$//)
+ports=$(sudo nmap -sU -Pn $1 -oN $1.udp.temp | grep ^[0-9] | cut -d '/' -f 1 | tr '\n' ',' | sed s/,$//)
 sudo nmap -n -v -Pn -sV -sC -oA udp-version -sU -p$ports -A -oN $1.udp
