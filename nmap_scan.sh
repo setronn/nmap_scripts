3bfc29c166a21aa4ae3d6d4bdd59145a42b1e8b2
setronn <setron34@gmail.com>
2022-10-18 03:56:18 -0400

    blocked dns resolution

--- nmap_scan.sh
+++ nmap_scan.sh
@@ -6,11 +6,11 @@ NC='\033[0m'
 dir=`echo $1 | tr '/' '-'` 
 
 echo -e "${RED}Scanning: $2 (TCP). Start time: $(date +%T)${NC}"
-nmap -Pn -p- $2 -oN scan_results/$dir/$2.tcp.tmp
+nmap -Pn -p- $2 -n -oN scan_results/$dir/$2.tcp.tmp
 ports=`cat scan_results/$dir/$2.tcp.tmp | grep ^[0-9] | cut -d '/' -f 1 | tr '\n' ',' | sed s/,$//`
 echo -e "${RED}Found ports: ${ports}${NC}"
 if [ ${#ports} != 0 ]; then
-    sudo nmap -p$ports -A $2 -oN scan_results/$dir/$2.tcp
+    sudo nmap -n -p$ports -A $2 -oN scan_results/$dir/$2.tcp
 fi
 echo -e "${RED}TCP scan is done after `date -d@$((\`date +%s\`-$startTime)) -u +%H:%M:%S` in $(date +%T)${NC}"
 echo ""
@@ -18,7 +18,7 @@ echo -e "${RED}=================================================================
 echo ""
 startTime=`date +%s`
 echo -e "${RED}Scanning: $2 (UDP). Start time: $(date +%T)${NC}"
-sudo nmap -sU -Pn $2 -oN scan_results/$dir/$2.udp.tmp
+sudo nmap -sU -Pn $2 -n -oN scan_results/$dir/$2.udp.tmp
 ports=`cat scan_results/$dir/$2.udp.tmp| grep ^[0-9] | cut -d '/' -f 1 | tr '\n' ',' | sed s/,$//`
 echo -e "${RED}Found ports: ${ports}${NC}"
 if [ ${#ports} != 0 ]; then
